6e9cd89bb68741b2e1993e29da250346216b3dc4
gxguinho <gxguinho@gmail.com>
2025-07-20 15:15:26 -0300

    feat: add recording audio

---
+++ src/http/use-room-questions.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import type { GetRoomQuestionsResponse } from "./types/get-room-questions-response";
+
+export function useRoomQuestions(roomId: string) {
+  const { data, isLoading } = useQuery({
+    queryKey: ["get-questions", roomId],
+    queryFn: async () => {
+      const response = await fetch(
+        `http://localhost:3333/rooms/${roomId}/questions`
+      );
+
+      const result: GetRoomQuestionsResponse = await response.json();
+
+      return result;
+    },
+  });
+
+  return { data, isLoading };
+}
